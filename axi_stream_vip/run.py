6cce0205dd4d6aba651a027fb401e8e341653f98
yangyt96 <yeeyang.tan@live.com>
2026-04-24 14:59:31 +0800

    [rmv] redundant lines

--- axi_stream_vip/run.py
+++ axi_stream_vip/run.py
@@ -3,11 +3,8 @@ from vunit import VUnit
 from pathlib import Path
 
 ROOT = Path(__file__).parents[0]
-DEFAULT_OUTPUT_PATH = Path("/tmp/axi_stream_vip_vunit_out")
 
 argv = list(sys.argv[1:])
-if "--output-path" not in argv:
-    argv.extend(["--output-path", str(DEFAULT_OUTPUT_PATH)])
 
 vu = VUnit.from_argv(argv=argv, compile_builtins=False)
 
@@ -19,7 +16,6 @@ lib.add_source_files(
     [
         ROOT / "*.sv",
     ],
-    # include_dirs=ROOT.as_posix()
 )
 
 
